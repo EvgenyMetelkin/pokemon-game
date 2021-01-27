ce72614225fca219999ab03305735fbaaf0d1a09
Evgen <metelkin.evgen@yandex.ru>
2021-01-27 22:54:22 +0300

    Homework 2

--- src/App.js
+++ src/App.js
@@ -1,17 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import HeaderBlock from './components/HeaderBlock/';
+import LayoutBlock from './components/LayoutBlock/';
+import FooterBlock from './components/FooterBlock/';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Hello World! 
-        </p> 
-      </header>
-    </div>
+    <>
+      <HeaderBlock 
+        title = "This is title"
+        descr = "This is Description!"
+      />
+      <LayoutBlock 
+        id = "firstLayout" 
+        title = "First"
+        desc = "First Layout"
+        urlBg = "../../assets/bg3.jpg"
+      />
+      <LayoutBlock 
+        id = "secondLayout" 
+        title = "Second"
+        desc = "Second Layout" 
+        colorBg = "#c6f5f0"
+      />
+      <LayoutBlock 
+        id = "thirdLayout" 
+        title = "Third"
+        desc = "Third Layout"
+        urlBg = "../../assets/bg3.jpg" 
+      />
+      <FooterBlock />
+    </>
   );
-}
+};
 
-export default App;
+export default App
